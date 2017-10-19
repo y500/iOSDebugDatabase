3cb850ef38c42420f3bdceeb0513adbe8a5502c8
yanqizhou <wentian@macbook.local>
2017-10-19 20:43:04 +0800

    add swift demo

---
+++ SwiftDebugDatabase/SwiftDebugDatabase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDebugDatabase
+//
+//  Created by wentian on 17/10/19.
+//  Copyright © 2017年 wentian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
